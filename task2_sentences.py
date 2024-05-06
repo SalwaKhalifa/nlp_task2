1ffb73a225475520a7304e7dfc570747e9a3c593
SalwaKhalifa <152662274+SalwaKhalifa@users.noreply.github.com>
2024-05-06 11:46:24 +0300

    task2_sentences.py

--- task2_sentences.py
+++ task2_sentences.py
@@ -8,20 +8,20 @@ df = pd.read_csv('korean_drama.csv')
 # Initialize SnowballStemmer for stemming
 stemmer = SnowballStemmer('english')
 
-# Tokenize the reviews into sentences
-tokenized_sentences = []
-for review in df['synopsis']:
-    sentences = sent_tokenize(str(review))
-    tokenized_sentences.extend(sentences)
+# Tokenize and stem the reviews
+def tokenize_and_stem(text):
+    tokenized_sentences = sent_tokenize(str(text))
+    stemmed_feedback = []
+    for sentence in tokenized_sentences:
+        words = word_tokenize(sentence)
+        stemmed_words = [stemmer.stem(word) for word in words]
+        stemmed_sentence = ' '.join(stemmed_words)
+        stemmed_feedback.append(stemmed_sentence)
+    return stemmed_feedback
 
-# Tokenize words, perform stemming, and store the results
-stemmed_feedback = []
-for sentence in tokenized_sentences:
-    words = word_tokenize(sentence)
-    stemmed_words = [stemmer.stem(word) for word in words]
-    stemmed_sentence = ' '.join(stemmed_words)
-    stemmed_feedback.append(stemmed_sentence)
+# Apply tokenization and stemming to each synopsis
+df['stemmed_synopsis'] = df['synopsis'].apply(tokenize_and_stem)
 
 # Print the first 10 tokenized and stemmed sentences
 for i in range(10):
-    print(stemmed_feedback[i])
+    print(df['stemmed_synopsis'][i])
